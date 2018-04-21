e9dcac9dbce06ab4336edbb5887e7e6172adcc1a
Rubin Taipi <rubin.taipi@gmail.com>
2018-04-21 14:25:51 -0400

    front end changes

--- citizensprogram/src/components/home.js
+++ citizensprogram/src/components/home.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Icon } from 'antd';
+import { Layout, Menu, Icon, Button} from 'antd';
 import 'antd/dist/antd.css';
+import Buddtable from './buddtable.js'
+import Peerbuddies from './peerbuddies'
 const { Header, Content, Footer, Sider } = Layout;
 
 class Home extends Component{
+  constructor(props){
+    super(props)
+    this.state= {
+      activeMenu: 1 
+    }
+  }
+
 
   render(){
+   const hide = {display: 'none'}
+   const show = {display: 'block'}   
     return (
       <Layout>
         <Sider style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
@@ -13,43 +24,20 @@ class Home extends Component{
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
             <Menu.Item key="1">
               <Icon type="user" />
-              <span className="nav-text">nav 1</span>
+              <span className="nav-text">Buddies</span>
             </Menu.Item>
             <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span className="nav-text">nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span className="nav-text">nav 3</span>
-            </Menu.Item>
-            <Menu.Item key="4">
-              <Icon type="bar-chart" />
-              <span className="nav-text">nav 4</span>
-            </Menu.Item>
-            <Menu.Item key="5">
-              <Icon type="cloud-o" />
-              <span className="nav-text">nav 5</span>
-            </Menu.Item>
-            <Menu.Item key="6">
-              <Icon type="appstore-o" />
-              <span className="nav-text">nav 6</span>
-            </Menu.Item>
-            <Menu.Item key="7">
-              <Icon type="team" />
-              <span className="nav-text">nav 7</span>
-            </Menu.Item>
-            <Menu.Item key="8">
-              <Icon type="shop" />
-              <span className="nav-text">nav 8</span>
-            </Menu.Item>
+              <Icon type="user"/>
+              <span className="nav-text">Peer Buddies</span>
+            </Menu.Item>          
           </Menu>
         </Sider>
         <Layout style={{ marginLeft: 200 }}>
-          <Header style={{ background: '#fff', padding: 0 }} />
+          <div style={{ background: '#fff', padding: 10, fontSize: 30 }}><b>Best Buddie</b></div>
           <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
             <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
-              
+              <Buddtable />
+              <Peerbuddies />
             </div>
           </Content>
           <Footer style={{ textAlign: 'center' }}>
