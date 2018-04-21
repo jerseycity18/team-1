688971d02d1456cf5f6bb1542ea2ad69a0d49371
Vaibhav <letmeshop@outlook.com>
2018-04-21 14:38:27 -0400

    adding more

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @org.springframework.web.bind.annotation.RestController
+@ImportResource("classpath:appcontext-config.xml")
 public class DemoApplication {
 
 	@RequestMapping("/")
