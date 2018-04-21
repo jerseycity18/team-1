56cdd9a63c63332c3c25bdd104060c98d2392187
Vaibhav <letmeshop@outlook.com>
2018-04-21 12:51:29 -0400

    initial code submit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@org.springframework.web.bind.annotation.RestController
+public class DemoApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	@RequestMapping("/listBuddies")
+	@ResponseBody
+	test listBuddies() {
+
+		return new test();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	class test{
+		private String m="v";
+
+		public String getM() {
+			return m;
+		}
+
+		public void setM(String m) {
+			this.m = m;
+		}
+	}
+}
